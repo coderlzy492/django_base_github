88dcf3ad7ca07e6431acda9c7e7114bef61d10e1
coderlzy492 <1062504912@qq.com>
2021-05-24 00:14:01 +0800

    在Django管理平台中配置两个创建好的模型类

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from book.models import BookInfo, CharacterInfo
 # Register your models here.
+
+admin.site.register(BookInfo)
+admin.site.register(CharacterInfo)
