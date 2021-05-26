27ec7afeeaddd7f1c274fb7f50ddf82db6d7fc88
coderlzy492 <1062504912@qq.com>
2021-05-27 07:48:54 +0800

    导入新的方法&添加新的路由

--- bookmanager/book/urls.py
+++ bookmanager/book/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from book.views import index
+from book.views import index, create_book
 
 urlpatterns = [
     # path(路由, 视图函数)
-    path('index/', index)
+    path('index/', index),
+    path('create/', create_book)
 ]
