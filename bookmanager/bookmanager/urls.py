2af21c0bf4eec2c3a0905127469461b31a506f6f
coderlzy492 <1062504912@qq.com>
2021-05-27 07:44:15 +0800

    Revert "添加路由配置信息"
    This reverts commit 33dd14c53f0637711fb4bdd3913f347acdd53070.

--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from book.views import index
+from book.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
