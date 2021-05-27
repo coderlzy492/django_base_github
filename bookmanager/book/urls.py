98b9549cfaf4eedd7724fe382a8782b160b44c77
coderlzy492 <1062504912@qq.com>
2021-05-27 18:14:25 +0800

    增加路由配置信息

--- bookmanager/book/urls.py
+++ bookmanager/book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from book.views import index, create_book
+from book.views import index, create_book, shop
 
 urlpatterns = [
     # path(路由, 视图函数)
     path('index/', index),
-    path('create/', create_book)
+    path('create/', create_book),
+    path('<province_id>/<city_id>', shop)
 ]
