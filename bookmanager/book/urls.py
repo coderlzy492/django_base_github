0a19af61d9a01aca96a1e96da4449c967afb3102
coderlzy492 <1062504912@qq.com>
2021-05-27 21:55:39 +0800

    新增register & register_json两个路由

--- bookmanager/book/urls.py
+++ bookmanager/book/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from book.views import index, create_book, shop
+from book.views import index, create_book, shop, register, register_json
 
 urlpatterns = [
     # path(路由, 视图函数)
     path('index/', index),
     path('create/', create_book),
-    path('<province_id>/<city_id>/', shop)
+    path('<province_id>/<city_id>/', shop),
+    path('register/', register),
+    path('register_json/', register_json)
 ]
