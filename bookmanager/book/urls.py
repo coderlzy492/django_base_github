e7095f022c684986480f62155755c913580eb083
coderlzy492 <1062504912@qq.com>
2021-06-01 06:07:42 +0800

    新增LoginView匹配路由信息

--- bookmanager/book/urls.py
+++ bookmanager/book/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.urls.converters import register_converter
 from book.views import index, create_book, shop, register_mobile, register, register_json, response, response_info, \
-    redirect_url, set_cookies, get_cookies, set_sessions, get_sessions
+    redirect_url, set_cookies, get_cookies, set_sessions, get_sessions, login, LoginView
 
 
 # 1.自定义手机号验证转换器
@@ -46,4 +46,8 @@ urlpatterns = [
     path('get_cookies/', get_cookies),
     path('set_sessions/', set_sessions),
     path('get_sessions/', get_sessions),
+    path('login/', login),
+
+    # <------------视图函数------------>
+    path('login_demo/', LoginView.as_view())
 ]
