33e8668324bb99bb66fc9d205dc034b52e13d962
coderlzy492 <1062504912@qq.com>
2021-06-01 17:36:56 +0800

    新增login_demo & order_demo两个路由配置

--- bookmanager/book/urls.py
+++ bookmanager/book/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.urls.converters import register_converter
 from book.views import index, create_book, shop, register_mobile, register, register_json, response, response_info, \
-    redirect_url, set_cookies, get_cookies, set_sessions, get_sessions, login, LoginView
+    redirect_url, set_cookies, get_cookies, set_sessions, get_sessions, login, LoginView, MyOrderView
 
 
 # 1.自定义手机号验证转换器
@@ -48,6 +48,9 @@ urlpatterns = [
     path('get_sessions/', get_sessions),
     path('login/', login),
 
-    # <------------视图函数------------>
-    path('login_demo/', LoginView.as_view())
+    # <------------LoginView视图函数------------>
+    path('login_demo/', LoginView.as_view()),
+
+    # <------------OrderView视图函数------------>
+    path('order_demo/', MyOrderView.as_view())
 ]
