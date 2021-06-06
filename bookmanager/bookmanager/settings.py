281bb1ff47c489c531d6c7c590f0f60d62b4464f
coderlzy492 <1062504912@qq.com>
2021-06-06 10:51:44 +0800

    把django-redis配置信息注释掉，保证admin站点可以正常登陆

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -142,3 +142,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_files'),
 ]
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379/1',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+# SESSION_CACHE_ALIAS = 'default'
