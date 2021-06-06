6dd79fbac6a4c2c9420d5ab9ac5d41471d5945ff
coderlzy492 <1062504912@qq.com>
2021-06-06 11:00:05 +0800

    把django-redis配置信息注释掉 & 还原关于LoginRequiredMixin类视图(多继承)的基本使用说明节点

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -144,14 +144,14 @@ STATICFILES_DIRS = [
 ]
 
 # 在settings文件中配置django-redis
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-SESSION_CACHE_ALIAS = 'default'
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://127.0.0.1:6379/1',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+# SESSION_CACHE_ALIAS = 'default'
